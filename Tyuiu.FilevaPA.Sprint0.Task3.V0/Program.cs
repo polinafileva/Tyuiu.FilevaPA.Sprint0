9fd06f526640434c3e842a0d5bce77a96ad3421f
Полина Филeва <polina_fileva@list.ru>
2025-09-27 18:30:33 +0500

    Task3

---
+++ Tyuiu.FilevaPA.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using Tyuiu.FilevaPA.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.FilevaPA.Sprint0.Task3.V0;
+
+internal class Program
+
+
+{
+   
+    public Program()
+    {
+    }
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(DataService.Sum(7, 7));
+        Console.ReadKey();
+    }
+}
